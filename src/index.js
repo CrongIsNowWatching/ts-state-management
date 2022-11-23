3317cdeed4dd37079c2b58b8e7aa8408ed3e5794
su100 <zxctys@naver.com>
2022-11-23 21:46:04 +0900

    step2: add reducer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import { createStore } from './redux';
 
+function reducer(state = {}, { type }) {
+  switch (type) {
+    case 'init':
+      return {
+        ...state,
+        count: 0,
+      };
+    default:
+      return { ...state };
+  }
+}
+
+// store 생성
 const store = createStore();
+
+// init 액션 실행
+store.dispatch({ type: 'init' });
+
 console.log(store.getState());
--- src/redux.js
+++ src/redux.js
@@ -1,8 +1,18 @@
-export function createStore() {
+export function createStore(reducer) {
   let state;
 
   const getState = () => ({ ...state });
+
+  // store에 접근해 상태값 업데이트
+  const dispatch = (action) => {
+    state = reducer(state, action);
+  };
+
+  // 상태값 초기화
+  state = reducer(state, {});
+
   return {
     getState,
+    dispatch,
   };
 }
